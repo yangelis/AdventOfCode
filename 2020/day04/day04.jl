67edf983bb993a8e5ff960c0c0ac53ec00c9f37c
Iaggelis <john_agelis@hotmail.com>
2020-12-04 19:18:35 +0200

    Refactor the whole code and finish part2

--- 2020/day04/day04.jl
+++ 2020/day04/day04.jl
@@ -1,63 +1,117 @@
-
-function parse_passports(lines::Vector{String})
-    dict = Vector{Dict{String, String}}()
-    for line in lines
-        temp_dict = Dict{String, String}()
-        matches = collect(eachmatch(r"(\w{3}):(#?\w{1,9})", line))
-        for c in matches
-            temp_dict[String(c.captures[1])] = String(c.captures[2])
-        end
-        push!(dict, temp_dict)
-    end
-    dict
+mutable struct Passport
+    byr::String
+    iyr::String
+    eyr::String
+    hgt::String
+    hcl::String
+    ecl::String
+    pid::String
+    cid::String
+    Passport() = new("","","","","","","","")
+    Passport(byr,iyr,eyr,hgt,hcl, ecl, pid, cid) =
+        new(byr,iyr,eyr,hgt,hcl, ecl, pid, cid)
 end
 
-function count_valid_passports(passports::Vector{Dict{String, String}})
-    counter::Int64 = 0
+function is_valid(pass::Passport)::Bool
+    for i in 1:(length(fieldnames(Passport))-1)
+        if isempty(getfield(pass, i))
+            return false
+        end
+    end
+    return true
+end
 
-    for passport in passports
-        if passport.count == 8
-            counter += 1
-        elseif !haskey(passport, "cid") && passport.count == 7
-            counter += 1
+function is_valid2(pass::Passport)::Bool
+    if !is_valid(pass)
+        return false
+    end
+
+    # check birth, issue and expiration year
+    if !(1920 <= parse(Int64, pass.byr) <= 2002) ||
+        !(2010 <= parse(Int64, pass.iyr) <= 2020) ||
+        !(2020 <= parse(Int64, pass.eyr) <= 2030)
+        return false
+    end
+
+    # check height
+    units = pass.hgt[end-1:end]
+    if units == "cm"
+        if !(150 <= parse(Int64, pass.hgt[1:end-2]) <= 193)
+           return false
+        end
+    elseif units == "in"
+        if !(59 <= parse(Int64, pass.hgt[1:end-2]) <= 76)
+            return false
+        end
+    else
+        return false
+    end
+
+    # check hair color
+    if pass.hcl[1] != '#' || length(pass.hcl) != 7
+        return false
+    end
+
+    for h in pass.hcl[2:end]
+        if !('0' <= h <= '9') && !('a' <= h <= 'f')
+            return false
         end
     end
 
-    return counter
+    # check eye color
+    avail_color = ["amb", "blu", "brn", "gry", "grn", "hzl", "oth"]
+    if !(pass.ecl in avail_color)
+        return false
+    end
+
+    # check passport ID
+    if length(pass.pid) != 9 || !any(isdigit, pass.pid)
+       return false
+    end
+
+    return true
 end
 
 function main(filename::String)
-    lines = readlines(filename)
-
-    clean_lines = Vector{Vector{String}}()
-    temp = Vector{String}()
-    for i in 1:length(lines)
-        if !isempty(lines[i])
-            push!(temp, lines[i])
-        elseif isempty(lines[i])
-            push!(clean_lines, temp)
-            temp = Vector{String}()
-        end
-
-        if i == length(lines)
-            push!(clean_lines, temp)
-            temp = Vector{String}()
-        end
+    lines = ""
+    for l in eachline(filename, keep=true)
+        lines *= l
     end
 
-    pass = Vector{String}(undef, length(clean_lines))
-    fill!(pass, "")
-    for i in 1:length(clean_lines)
-        for element in clean_lines[i]
-            pass[i] *= element * " "
-       end
+    lines = replace(replace(replace(lines,"\n\n"=>"+"), "\n"=>" "), "+"=>'\n')
+
+    lines = split(lines, '\n')
+
+    passports = Vector{Passport}(undef, length(lines))
+    for (i, line) in enumerate(lines)
+        part_line = split(line, ' ')
+
+        temp_pass = Passport()
+        for part in part_line
+            if findfirst("byr", part) !== nothing
+                temp_pass.byr = String(part[5:end])
+            elseif findfirst("iyr", part) !== nothing
+                temp_pass.iyr = String(part[5:end])
+            elseif findfirst("eyr", part) !== nothing
+                temp_pass.eyr = String(part[5:end])
+            elseif findfirst("hgt", part) !== nothing
+                temp_pass.hgt = String(part[5:end])
+            elseif findfirst("hcl", part) !== nothing
+                temp_pass.hcl = String(part[5:end])
+            elseif findfirst("ecl", part) !== nothing
+                temp_pass.ecl = String(part[5:end])
+            elseif findfirst("pid", part) !== nothing
+                temp_pass.pid = String(part[5:end])
+            elseif findfirst("cid", part) !== nothing
+                temp_pass.cid = String(part[5:end])
+            end
+        end
+
+        passports[i] = temp_pass
     end
 
 
-    dict = parse_passports(pass)
-
-    valid = count_valid_passports(dict)
-    println("Part1: ", valid)
-
+    println("Part1: ", length(filter(is_valid, passports)))
+    println("Part2: ", length(filter(is_valid2, passports)))
 
 end
