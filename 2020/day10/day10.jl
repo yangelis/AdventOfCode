734385789d9d6d5c4369a8e59402c6223464782c
Iaggelis <john_agelis@hotmail.com>
2020-12-10 11:32:49 +0200

    Day10 part2 solution

--- 2020/day10/day10.jl
+++ 2020/day10/day10.jl
@@ -1,23 +1,11 @@
-
-
-
-
-
-
 function part1(adapters::Vector{Int64})
-    sort!(adapters)
-
-    last_adapter = adapters[end] + 3
-    push!(adapters, last_adapter)
-
-    ones=0
-    threes=0
+    ones = 0
+    threes = 0
     i = 1
 
     init_jolt = 0
 
     while i <= length(adapters)
-
         if (adapters[i] - init_jolt) == 3
             init_jolt = adapters[i]
             threes += 1
@@ -32,17 +20,23 @@ function part1(adapters::Vector{Int64})
     return ones * threes
 end
 
+function part2(adapters::Vector{Int64})
+    N = length(adapters)
+    c = zeros(Int64, N)
+    c[1] = 1
 
-
-
-
-
-
-
+    for i = 1:N, j = i+1:N
+        if (adapters[j] - adapters[i]) <= 3
+            c[j] += c[i]
+        end
+    end
+    return c[end]
+end
 
 function main(filename::String)
-    lines = readlines(filename) |> x->map(f->parse(Int64, f), x)
+    lines = readlines(filename) |> x->map(f->parse(Int64, f), x) |>
+        x->sort!(x) |> x->pushfirst!(x, 0) |> x->push!(x, x[end]+3)
 
     println("Part1: ", part1(lines))
-
+    println("Part2: ", part2(lines))
 end
