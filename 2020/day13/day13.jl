2130b1f89c4d2a1997453e7aae2e4c44c1413621
Iaggelis <john_agelis@hotmail.com>
2020-12-13 13:08:26 +0200

    Day 13 part2

--- 2020/day13/day13.jl
+++ 2020/day13/day13.jl
@@ -1,4 +1,29 @@
-function part1(timestamp, ids)
+# almost crt
+function chineseremainder(ni::Array, ci::Array)
+    N = BigInt(prod(ni))
+    Ni = map(x->N÷x, ni)
+    di = map(x->invmod(x[1], x[2]), zip(Ni, ni))
+    mod(sum(ci[i] * di[i] * Ni[i] for i in 1:length(ni)), N)
+    mod(reduce(-, ci[i] * di[i] * Ni[i] for i in 1:length(ni)), N)
+end
+
+function part2(buses)
+    position = findall(x->x!=nothing, tryparse.(Int, split(buses, ","))) .- 1
+    ids = buses |>
+        x->split(x,',') |>
+        l->filter(x->x!="x",l) |>
+        x->parse.(Int64, x)
+
+    chineseremainder(ids, position)
+end
+
+
+function part1(timestamp, buses)
+    ids = buses |>
+        x->split(x,',') |>
+        l->filter(x->x!="x",l) |>
+        x->parse.(Int64, x)
+
     initial_timestamp = timestamp
 
     while true
@@ -7,7 +32,7 @@ function part1(timestamp, ids)
         if index === nothing
             timestamp += 1
         else
-            println((timestamp, ids[index]))
+            # println((timestamp, ids[index]))
             return ids[index] * (timestamp-initial_timestamp)
         end
 
@@ -17,11 +42,8 @@ end
 function main(filename::String)
     lines = readlines(filename)
     timestamp = lines[1] |> x->parse(Int64,x)
-    ids = lines[2] |>
-        x->split(x,',') |>
-        l->filter(x->x!="x",l) |>
-        x->parse.(Int64, x)
 
 
-    println("Part1: ", part1(timestamp, ids))
+    println("Part1: ", part1(timestamp, lines[2]))
+    println("Part2: ", part2(lines[2]))
 end
