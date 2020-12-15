6429129ff815a065c3600d524fec882f585e62aa
Iaggelis <john_agelis@hotmail.com>
2020-12-16 00:52:20 +0200

    add day01 in c++

---
+++ 2020/day01/day01.cpp
@@ -0,0 +1,45 @@
+#include "../helpers.hpp"
+#include <algorithm>
+
+using namespace std;
+
+int part1(vector<int> &lines) {
+
+  const size_t N = lines.size();
+  for (size_t i = 0; i < N; ++i) {
+    auto l2 = find(lines.begin(), lines.end(), 2020 - lines[i]);
+    if (l2 != end(lines)) {
+      return lines[i] * (*l2);
+    }
+  }
+  return -1;
+}
+
+int part2(vector<int> &lines) {
+
+  const size_t N = lines.size();
+  for (size_t i = 0; i < N - 1; ++i) {
+    for (size_t j = i; j < N; ++j) {
+      auto l3 = find(lines.begin(), lines.end(), 2020 - lines[i] - lines[j]);
+      if (l3 != end(lines)) {
+        return lines[i] * lines[j] * (*l3);
+      }
+    }
+  }
+  return -1;
+}
+
+int main(int argc, char *argv[]) {
+  if (argc == 1){
+    cerr << "No input file given\n";
+    exit(1);
+  }
+
+  auto lines = helpers::map(helpers::read_lines_as_string_view(argv[1]),
+                            helpers::to_int);
+
+  cout << part1(lines) << '\n';
+  cout << part2(lines) << '\n';
+
+  return 0;
+}
